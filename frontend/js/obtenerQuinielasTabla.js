75a49f79b5480e0cc98f7a36bdb24f1000a2f71b
Kevin <keelamezcua@ittepic.edu.mx>
2025-09-27 18:12:22 -0700

    Update obtenerQuinielasTabla.js

--- frontend/js/obtenerQuinielasTabla.js
+++ frontend/js/obtenerQuinielasTabla.js
@@ -2,9 +2,9 @@ const apiURL = 'https://quinielamx.onrender.com';
 
 const resultadosOficiales = [
     "local", // Partido 1
-    "", // Partido 2
-    "", // Partido 3
-    "", // Partido 4
+    "visita", // Partido 2
+    "local", // Partido 3
+    "local", // Partido 4
     "", // Partido 5
     "", // Partido 6
     "", // Partido 7
